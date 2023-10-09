448f6af9e5d31cccae4b05b9d91d2f8c62b1862c
rons9n <rons9n@gmail.com>
2023-10-09 18:29:39 -0500

    Columns component, Prisma setup with Course model

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,7 +1,10 @@
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+import { db } from "@/lib/db";
+
 import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
 
 const CoursesPage = async () => {
   const { userId } = auth();
@@ -10,9 +13,18 @@ const CoursesPage = async () => {
     return redirect("/");
   }
 
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="p-6">
-      <DataTable />
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
