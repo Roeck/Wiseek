99e7b317dc58b0ab1b0bf6b6f3a22dbe75b81370
rons9n <rons9n@gmail.com>
2023-10-21 16:04:51 -0500

    Purchase model and get-analytics action

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,12 +1,16 @@
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+import { getAnalytics } from "@/actions/get-analytics";
+
 const AnalyticsPage = async () => {
   const { userId } = auth();
 
   if (!userId) {
     return redirect("/");
   }
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
 
   return (
     <div className="p-6">
