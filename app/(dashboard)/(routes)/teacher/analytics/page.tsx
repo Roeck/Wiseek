ba78dfd89cd18fcbee1373a801a9ce8ad2437abd
rons9n <rons9n@gmail.com>
2023-10-22 16:09:24 -0500

    Data-card and Card components

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -3,6 +3,8 @@ import { redirect } from "next/navigation";
 
 import { getAnalytics } from "@/actions/get-analytics";
 
+import { DataCard } from "./_components/data-card";
+
 const AnalyticsPage = async () => {
   const { userId } = auth();
 
@@ -14,8 +16,10 @@ const AnalyticsPage = async () => {
 
   return (
     <div className="p-6">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">DataCard</div>
-      Chart
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard label="Total Revenue" value={totalRevenue} shouldFormat />
+        <DataCard label="Total Sales" value={totalSales} />
+      </div>
     </div>
   );
 };
