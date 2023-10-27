a25dcf436074a2aa83ac790f1c37a3872d9b1c89
rons9n <rons9n@gmail.com>
2023-10-26 20:45:24 -0500

    SearchPage setup

---
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -0,0 +1,40 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { SearchInput } from "@/components/search-input";
+
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  return (
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+      <div className="p-6 space-y-4">
+        <h1>Categories</h1>
+        <h1>CourseList</h1>
+      </div>
+    </>
+  );
+};
+
+export default SearchPage;
