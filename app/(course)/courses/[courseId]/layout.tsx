bb1621a6536e3a42e949953b081c8227c415bbb0
rons9n <rons9n@gmail.com>
2023-10-28 15:08:14 -0500

    CourseId layout page

---
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -0,0 +1,62 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { getProgress } from "@/actions/get-progress";
+
+const CourseLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { courseId: string };
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+      chapters: {
+        where: {
+          isPublished: true,
+        },
+        include: {
+          userProgress: {
+            where: {
+              userId,
+            },
+          },
+        },
+        orderBy: {
+          position: "asc",
+        },
+      },
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  const progressCount = await getProgress(userId, course.id);
+
+  return (
+    <div className="h-full">
+      <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
+        CourseNavbar
+      </div>
+      <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
+        CourseSidebar
+      </div>
+      <main className="md:pl-80 pt-[80px] h-full">{children}</main>
+    </div>
+  );
+};
+
+export default CourseLayout;
