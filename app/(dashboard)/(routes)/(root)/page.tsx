bf91a7f055bc8eca1169a1089de75a881561a381
rons9n <rons9n@gmail.com>
2023-10-30 18:20:06 -0500

    Get-dashboard-courses action

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { CheckCircle, Clock } from "lucide-react";
 
+import { getDashboardCourses } from "@/actions/get-dashboard-courses";
 import { CoursesList } from "@/components/courses-list";
 
 import { InfoCard } from "./_components/info-card";
@@ -12,6 +13,10 @@ export default async function Dashboard() {
   if (!userId) {
     return redirect("/");
   }
+
+  const { completedCourses, coursesInProgress } = await getDashboardCourses(
+    userId
+  );
 
   return (
     <div className="p-6 space-y-4">
