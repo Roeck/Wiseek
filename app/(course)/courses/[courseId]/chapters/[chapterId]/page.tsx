f0b93201069c96e88b758fa609dcd43308348e63
rons9n <rons9n@gmail.com>
2023-10-30 15:56:10 -0500

    VideoPlayer component

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -3,6 +3,8 @@ import { redirect } from "next/navigation";
 import { File } from "lucide-react";
 
 import { Preview } from "@/components/preview";
+import { Banner } from "@/components/banner";
+import { VideoPlayer } from "./_components/video-player";
 
 const ChapterIdPage = async ({
   params,
@@ -15,10 +17,43 @@ const ChapterIdPage = async ({
     return redirect("/");
   }
 
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchase,
+  } = await getChapter({
+    userId,
+    chapterId: params.chapterId,
+    courseId: params.courseId,
+  });
+
+  if (!chapter || !course) {
+    return redirect("/");
+  }
+
+  const isLocked = !chapter.isFree && !purchase;
+  const completeOnEnd = !!purchase && !userProgress?.isCompleted;
+
   return (
     <div>
+      <Banner variant="success" label="You already completed this chapter." />
       <div className="flex flex-col max-w-4xl mx-auto pb-20">
-        <div className="p-4">VideoPlayer</div>
+        <div className="p-4">
+          {" "}
+          <VideoPlayer
+            chapterId={params.chapterId}
+            title={chapter.title}
+            courseId={params.courseId}
+            nextChapterId={nextChapter?.id}
+            playbackId={muxData?.playbackId!}
+            isLocked={isLocked}
+            completeOnEnd={completeOnEnd}
+          />
+        </div>
         <div>
           <div className="p-4 flex flex-col md:flex-row items-center justify-between">
             <h2 className="text-2xl font-semibold mb-2">Chapter title</h2>
