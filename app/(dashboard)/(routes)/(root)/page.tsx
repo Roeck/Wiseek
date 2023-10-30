bb268f9e8eb318f7a6fccbe36e3f54c501780c3b
rons9n <rons9n@gmail.com>
2023-10-30 18:18:22 -0500

    Info-card component

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -4,6 +4,8 @@ import { CheckCircle, Clock } from "lucide-react";
 
 import { CoursesList } from "@/components/courses-list";
 
+import { InfoCard } from "./_components/info-card";
+
 export default async function Dashboard() {
   const { userId } = auth();
 
@@ -13,8 +15,20 @@ export default async function Dashboard() {
 
   return (
     <div className="p-6 space-y-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">InfoCards</div>
-      CoursesList
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <InfoCard
+          icon={Clock}
+          label="In Progress"
+          numberOfItems={coursesInProgress.length}
+        />
+        <InfoCard
+          icon={CheckCircle}
+          label="Completed"
+          numberOfItems={completedCourses.length}
+          variant="success"
+        />
+      </div>
+      <CoursesList items={[...coursesInProgress, ...completedCourses]} />
     </div>
   );
 }
