e915fbea6be66732cf8df6b539d0ccad0d60a91c
rons9n <rons9n@gmail.com>
2023-10-30 15:21:20 -0500

    CourseSidebarItem component

--- app/(course)/courses/[courseId]/_components/course-mobile-sidebar.tsx
+++ app/(course)/courses/[courseId]/_components/course-mobile-sidebar.tsx
@@ -1,11 +1,13 @@
-import { Menu } from "lucide-react";
+import { auth } from "@clerk/nextjs";
 import { Chapter, Course, UserProgress } from "@prisma/client";
+import { redirect } from "next/navigation";
 
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { db } from "@/lib/db";
+import { CourseProgress } from "@/components/course-progress";
 
-import { CourseSidebar } from "./course-sidebar";
+import { CourseSidebarItem } from "./course-sidebar-item";
 
-interface CourseMobileSidebarProps {
+interface CourseSidebarProps {
   course: Course & {
     chapters: (Chapter & {
       userProgress: UserProgress[] | null;
@@ -14,18 +16,47 @@ interface CourseMobileSidebarProps {
   progressCount: number;
 }
 
-export const CourseMobileSidebar = ({
+export const CourseSidebar = async ({
   course,
   progressCount,
-}: CourseMobileSidebarProps) => {
+}: CourseSidebarProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const purchase = await db.purchase.findUnique({
+    where: {
+      userId_courseId: {
+        userId,
+        courseId: course.id,
+      },
+    },
+  });
+
   return (
-    <Sheet>
-      <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
-        <Menu />
-      </SheetTrigger>
-      <SheetContent side="left" className="p-0 bg-white w-72">
-        <CourseSidebar course={course} progressCount={progressCount} />
-      </SheetContent>
-    </Sheet>
+    <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
+      <div className="p-8 flex flex-col border-b">
+        <h1 className="font-semibold">{course.title}</h1>
+        {purchase && (
+          <div className="mt-10">
+            <CourseProgress variant="success" value={progressCount} />
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col w-full">
+        {course.chapters.map((chapter) => (
+          <CourseSidebarItem
+            key={chapter.id}
+            id={chapter.id}
+            label={chapter.title}
+            isCompleted={!!chapter.userProgress?.[0]?.isCompleted}
+            courseId={course.id}
+            isLocked={!chapter.isFree && !purchase}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
