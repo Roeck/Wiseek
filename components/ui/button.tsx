111bd69afce3b338447dbc71baeedbb83150b352
rons9n <rons9n@gmail.com>
2023-11-15 12:06:13 -0500

    Logo, favicon, button and name updates

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         success: "bg-emerald-600 text-white hover:bg-emerald-600/80",
+        classic: "bg-blue-400 text-white",
       },
       size: {
         default: "h-10 px-4 py-2",
