48d25663dfbdfbbf2c3d33a18113fbe414d3e7f2
Roeck <rons9n@gmail.com>
2024-09-05 19:00:20 -0400

    Navbar update

--- app/(course)/courses/[courseId]/_components/course-navbar.tsx
+++ app/(course)/courses/[courseId]/_components/course-navbar.tsx
@@ -2,7 +2,7 @@ import { Chapter, Course, UserProgress } from "@prisma/client";
 
 import { NavbarRoutes } from "@/components/navbar-routes";
 
-import { CourseSidebar } from "./course-sidebar";
+import { CourseMobileSidebar } from "./course-mobile-sidebar";
 
 interface CourseNavbarProps {
   course: Course & {
@@ -16,7 +16,7 @@ interface CourseNavbarProps {
 export const CourseNavbar = ({ course, progressCount }: CourseNavbarProps) => {
   return (
     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
-      <CourseSidebar course={course} progressCount={progressCount} />
+      <CourseMobileSidebar course={course} progressCount={progressCount} />
       <NavbarRoutes />
     </div>
   );
